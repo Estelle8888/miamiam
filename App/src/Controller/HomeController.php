bfb3452f942a455c60b450082a11a39519fd9b0a
Estelle Lefebvre <estelle.lfb@hotmail.com>
2020-11-09 17:46:44 +0100

    Insertion de recettes avec persist et flush

--- App/src/Controller/HomeController.php
+++ App/src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,10 +16,34 @@ class HomeController extends AbstractController{
  * @Route ("/" , name="home")
  */
 
+ 
+
     //la fonction index va nous permettre d'afficher la page d'acceuil
-    public function index(){
+    public function index(EntityManagerInterface $em){
+        $recette= new Recette();
+        $recette
+            ->setTitle('Doukouna')
+            ->setDescription('description')
+            ->setPreparation('In his tractibus navigerum nusquam visitur flumen sed in locis plurimis aquae suapte natura calentes emergunt ad usus aptae multiplicium medelarum. verum has quoque regiones pari sorte Pompeius Iudaeis domitis et Hierosolymis captis in provinciae speciem delata iuris dictione formavit.')
+            ->setTime('120 minutes')
+            ->setPersonne(4)
+            ->setCreatedAt(new DateTime());
+
+        $recette2= new Recette();
+        $recette2
+            ->setTitle('Doukouna')
+            ->setDescription('description')
+            ->setPreparation('In his tractibus navigerum nusquam visitur flumen sed in locis plurimis aquae suapte natura calentes emergunt ad usus aptae multiplicium medelarum. verum has quoque regiones pari sorte Pompeius Iudaeis domitis et Hierosolymis captis in provinciae speciem delata iuris dictione formavit.')
+            ->setTime('120 minutes')
+            ->setPersonne(4)
+            ->setCreatedAt(new DateTime());
+
+            //persist : l'intention de... insérer dans la base de données
+            $em->persist($recette2);
+            $em->flush();
         return $this->render('/pages/home.html.twig');
     }
+    
 }
 
 ?>
--- App/src/Controller/RecetteController.php
+++ App/src/Controller/RecetteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
